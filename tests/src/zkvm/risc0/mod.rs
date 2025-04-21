1f3efe8a4c56b4d388a5e4aee940ded5d2dc9777
Preston Ong <preston4896@me.com>
2025-04-21 09:55:43 +0800

    use cargo manifest path

--- tests/src/zkvm/risc0/mod.rs
+++ tests/src/zkvm/risc0/mod.rs
@@ -8,22 +8,28 @@ use anchor_client::{
 use anyhow::Result;
 use super::deploy_program;
 
-/// The path to the Groth16 verifier program binary
-pub const GROTH16_VERIFIER_SO_PATH: &str = "src/zkvm/risc0/so/groth_16_verifier.so";
-
-/// The path to the Groth16 verifier program keypair
-pub const GROTH16_VERIFIER_KEYPAIR_PATH: &str = "src/zkvm/risc0/so/groth_16_verifier-test-keypair.json";
-
 /// Deploy the RISC0 Groth16 verifier program to the test validator
 pub async fn deploy_risc0_groth16_verifier(
     payer: &Keypair,
     client: &RpcClient,
 ) -> Result<Pubkey> {
+    let groth16_verifier_path = format!(
+        "{}/{}",
+        env!("CARGO_MANIFEST_DIR"),
+        "src/zkvm/risc0/so/groth_16_verifier.so"
+    );
+
+    let groth16_verifier_keypair_path = format!(
+        "{}/{}",
+        env!("CARGO_MANIFEST_DIR"),
+        "src/zkvm/risc0/so/groth_16_verifier-test-keypair.json"
+    );
+    
     // Use the centralized deployment function
     deploy_program(
         payer, 
         client, 
-        GROTH16_VERIFIER_SO_PATH,
-        read_keypair_file(GROTH16_VERIFIER_KEYPAIR_PATH).unwrap(),
+        &groth16_verifier_path,
+        read_keypair_file(&groth16_verifier_keypair_path).unwrap(),
     ).await
 }
